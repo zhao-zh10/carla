60314a97e8a2a94938d49add369f53cbe6a3beb1
Daniel <dnovillo@cvc.uab.es>
2019-02-18 17:40:40 +0100

    List all maps from Carla Statics. Load map from full path or name.

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Game/CarlaEpisode.h
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Game/CarlaEpisode.h
@@ -11,6 +11,7 @@
 #include "Carla/Weather/Weather.h"
 
 #include "GameFramework/Pawn.h"
+#include "Kismet/GameplayStatics.h"
 
 #include <compiler/disable-ue4-macros.h>
 #include <carla/rpc/Actor.h>
@@ -171,6 +172,50 @@ public:
   bool DestroyActor(AActor *Actor)
   {
     return ActorDispatcher->DestroyActor(Actor);
+  }
+
+  // ===========================================================================
+  // -- World handling methods -------------------------------------------------
+  // ===========================================================================
+
+  UFUNCTION(BlueprintCallable)
+  bool LoadMap(const FString& MapString, const UObject* WorldContext) {
+    bool bIsFileFound = false;
+    FString FinalPath = MapString;
+
+
+    if (MapString.StartsWith("/Game")) {
+      //Full path
+        if(!MapString.EndsWith(".umap")) {
+          FinalPath += ".umap";
+        }
+        //Some conversions...
+        FinalPath = FinalPath.Replace(TEXT("/Game/"), *FPaths::ProjectContentDir());
+        if(FPaths::FileExists(IFileManager::Get().ConvertToAbsolutePathForExternalAppForRead(*FinalPath))) {
+          bIsFileFound = true;
+          FinalPath = MapString;
+        }
+    } else {
+      if(MapString.Contains("/")) bIsFileFound = false;
+      else {
+        //Find the full path under Carla
+        TArray<FString> TempStrArray, PathList;
+        if(!MapString.EndsWith(".umap")) {
+          FinalPath += ".umap";
+        }
+        IFileManager::Get().FindFilesRecursive(PathList, *FPaths::ProjectContentDir(), *FinalPath, true, false, false);
+        if(PathList.Num()>0) {
+          FinalPath = PathList[0];
+          FinalPath.ParseIntoArray(TempStrArray, TEXT("Content/"), true);
+          FinalPath = TempStrArray[1];
+          FinalPath.ParseIntoArray(TempStrArray, TEXT("."), true);
+          FinalPath = "/Game/" + TempStrArray[0];
+          bIsFileFound = true;
+        }
+      }
+    }
+    if(bIsFileFound) UGameplayStatics::OpenLevel(WorldContext, *FinalPath, true);
+    return bIsFileFound;
   }
 
   // ===========================================================================
